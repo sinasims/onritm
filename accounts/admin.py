54af89d17e76b8e360dbfe9656ca283ddb89d74b
sina <sina.rahmaniii@yahoo.com>
2026-04-24 19:36:02 +0330

    created user models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('اطلاعات اضافی', {
+            'fields': ('phone_number', 'date_of_birth', 'avatar')
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('اطلاعات اضافی', {
+            'fields': ('phone_number', 'date_of_birth', 'avatar')
+        }),
+    )
+    list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
